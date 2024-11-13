8bd4721c9166ac9e7bd3244ed55863ab7dcdfff8
Irene San Roman <irenesrfuentes@gmail.com>
2024-11-13 14:06:26 +0100

    Pipes Personalizados

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -36,8 +36,9 @@ export class MenuComponent {
         icon: 'pi pi-cog',
         items: [
           {
-          label: 'Otro Elemento',
+          label: 'Custom Pipes',
           icon: 'pi pi-cog',
+          routerLink: 'custom'
           }
         ]
       }
